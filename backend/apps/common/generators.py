4d73c2e77d57ab201dcb6e3230440bad0048dac4
igorkhaylov <igorkhaylov@yandex.com>
2025-10-06 14:01:00 +0500

    [IMP] add gerate_dates function

--- backend/apps/common/generators.py
+++ backend/apps/common/generators.py
@@ -1,6 +1,9 @@
 import random
 import secrets
 import string
+from datetime import datetime, timedelta
+
+import dateutil
 
 
 def generate_unique_string(length: int = 16) -> str:
@@ -24,3 +27,32 @@ def generate_random_verification_code(length=6):
     return str(random.randint(100, max_int)).rjust(
         length, "0"
     )  # return "999999".rjust(6, '0')
+
+
+def generate_dates(start_date=None, count=31, step=1, unit="days"):
+    """
+    Генерирует даты на основе заданного начального дня, количества дат, шага и единицы измерения шага.
+
+    :param start_date: Начальная дата'
+    :param count: Количество дат для генерации
+    :param step: Шаг между датами
+    :param unit: Единица измерения шага ('days', 'weeks', 'months', 'years')
+    :return: Список сгенерированных дат
+    """
+    if start_date is None:
+        start_date = datetime.now().date()
+    dates = [start_date]
+
+    for _ in range(1, count):
+        if unit == "days":
+            next_date = dates[-1] + timedelta(days=step)
+        elif unit == "weeks":
+            next_date = dates[-1] + timedelta(weeks=step)
+        elif unit == "months":
+            next_date = dates[-1] + dateutil.relativedelta.relativedelta(months=step)
+        elif unit == "years":
+            next_date = dates[-1] + dateutil.relativedelta.relativedelta(years=step)
+        else:
+            raise ValueError("Unit must be 'days', 'weeks', 'months', or 'years'")
+        dates.append(next_date)
+    return dates
