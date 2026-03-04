00b31050505e17dc9e4535399a26f5d71b9c545a
igorkhaylov <igorkhaylov@yandex.com>
2026-03-04 14:49:44 +0500

    [FIX] something

--- backend/config/settings/rest_framework.py
+++ backend/config/settings/rest_framework.py
@@ -14,7 +14,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 40,
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
